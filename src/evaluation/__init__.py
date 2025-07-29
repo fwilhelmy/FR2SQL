d2a2231e0fd64ee22820db7e5b6f1a5084d53ae8
Félix Wilhelmy <fwilhelmy@hotmail.com>
2025-07-28 20:37:08 -0400

    Clean of evaluation code

--- src/evaluation/__init__.py
+++
@@ -1,8 +0,0 @@
-
-"""Expose evaluation utilities."""
-
-from .Evaluator import Evaluator
-from .Picard import Picard
-from .pipeline_evaluator import evaluate_dataset
-
-__all__ = ["Evaluator", "Picard", "evaluate_dataset"]
